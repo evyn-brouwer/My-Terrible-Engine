67da917bfa3595c4736a0c42827e080325850e9f
Evyn <evyn.brouwer@ontariotechu.net>
2020-02-26 13:43:59 -0500

    SceneManager Comments

--- src/SceneStuff/SceneManager.cpp
+++ src/SceneStuff/SceneManager.cpp
@@ -13,17 +13,17 @@ void SceneManager::update(float dt)
 		}
 		else {//if the current scene wants to change
 			if (_currentScene->_newSceneName != _currentScene->_sceneName) {//if the scenes to change to is not this scene
-				if (_currentScene->_newSceneName!="") {
+				if (_currentScene->_newSceneName!="") {//if there is an actual scene name to change to
 					for (unsigned i = 0; i < _scenes.size(); i++) {//check all the scenes
-						if (_currentScene->_newSceneName == _scenes[i]->_sceneName) {
+						if (_currentScene->_newSceneName == _scenes[i]->_sceneName) {//if we have found the correct scene to change to
 							_currentScene->_newSceneName = "";
 							_currentScene->_changeScene = false;
-							_currentScene = _scenes[i];
+							_currentScene = _scenes[i];//change the scene
 							_currentScene->_changeScene = false;
 							_currentScene->_newSceneName = "";
 							_currentScene->update(dt);
 						}
-						else if (i == _scenes.size()-1) {
+						else if (i == _scenes.size() - 1) {
 							_currentScene->update(dt);
 							_currentScene->_changeScene = false;
 							std::cout << "There is no " << _currentScene->_newSceneName << " Scene!" << std::endl;
@@ -34,7 +34,7 @@ void SceneManager::update(float dt)
 				}
 				else {
 					_currentScene->update(dt);
-					std::cout << _currentScene->_sceneName << " wants to change scene, but doesn't know to what scene!" << std::endl;
+					std::cout << _currentScene->_sceneName << " wants to change scene, but doesn't know to what scene to change to!" << std::endl;
 					_currentScene->_changeScene = false;
 				}
 			}
